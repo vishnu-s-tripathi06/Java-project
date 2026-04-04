2ac9351e6ea375513896f61cc8981c88bd489429
Vishnu Shankar Tripathi <vstripathi400@gmail.com>
2026-04-04 20:41:57 +0530

    update 1.1

--- Main.java
+++ Main.java
@@ -1,14 +1,21 @@
 import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
-        System.out.print("hello world:  ");
-        int n=input.nextInt();
-        int num=0;
-        while (num<n){
-            System.out.println("Hello World!");
-            num+=1;
-        }
+    Scanner input=new Scanner(System.in);
+    System.out.println("Enter the index");
+    int num=input.nextInt();
+    int a=0;
+    int b=1;
+    int c=0;
+    for (int i=1;i<num;i++){
+        c=a+b;
+
+        a=b;
+        b=c;
+
+    }
+    System.out.println(c);
+    
     }
 }
 
